f0018e4007379b8154d447c060b53c0c013d88dc
Wills Blake <richwblake@gmail.com>
2019-12-03 21:42:14 -0500

    Final commit, all tests pass.

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,30 @@
 class Customer
-  
+  attr_reader :name, :age
+  @@all = []
+
+  def initialize(name, age)
+    @name = name
+    @age = age
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def new_meal(waiter, total, tip)
+    Meal.new(waiter, self, total, tip)
+  end
+
+  def meals
+    Meal.all.find_all { |meal| meal.customer == self  }
+  end
+
+  def waiters
+    waiter_array = []
+    Meal.all.each { |meal|
+      meal.customer == self ? waiter_array << meal.waiter : nil
+    }
+    waiter_array
+  end
 end
